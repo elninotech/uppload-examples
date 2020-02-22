da1e8506d1cced3bb39fd62b9d9b63f4f0041219
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-22 17:09:17 +0100

    :heavy_plus_sign: Add marked for MD compile

--- scripts/add-template.js
+++ scripts/add-template.js
@@ -1,8 +1,20 @@
 const { readFileSync, writeFileSync } = require("fs");
 const { join } = require("path");
+const marked = require("marked");
 
 const templatePath = join(__dirname, "..", "template.html");
-const templateHtml = readFileSync(templatePath, { encoding: "utf8" });
+let templateHtml = readFileSync(templatePath, { encoding: "utf8" });
+
+const readmePath = join(".", "README.md");
+try {
+  const readmeMd = readFileSync(readmePath, { encoding: "utf8" });
+  const readmeHtml = marked(readmeMd.toString())
+    .split("\n", 2)
+    .join("\n");
+  templateHtml = templateHtml
+    .toString()
+    .replace("<!-- Contents -->", readmeHtml);
+} catch (error) {}
 
 const indexPath = join(".", "index.html");
 writeFileSync(indexPath, templateHtml);
