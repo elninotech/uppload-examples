55bf57bfeafd5171c9fa10bf46a51a4c02b649c0
Anand Chowdhary <mail@anandchowdhary.com>
2020-02-22 16:38:34 +0100

    :see_no_evil: Add index.html to .gitignore

--- scripts/add-template.js
+++ scripts/add-template.js
@@ -1,1 +1,10 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
 
+const templatePath = join(__dirname, "..", "template.html");
+const templateHtml = readFileSync(templatePath, { encoding: "utf8" });
+
+const indexPath = join(".", "index.html");
+writeFileSync(indexPath, templateHtml);
+
+console.log("Written index.html");
