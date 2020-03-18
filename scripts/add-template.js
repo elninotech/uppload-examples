98712f021d0837ebc25fd205b81bd4ef0eee0d62
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-18 23:41:43 +0530

    :recycle: Add HTML in template

--- scripts/add-template.js
+++ scripts/add-template.js
@@ -2,8 +2,54 @@ const { readFileSync, writeFileSync } = require("fs");
 const { join, resolve } = require("path");
 const marked = require("marked");
 
-const templatePath = join(__dirname, "..", "template.html");
-let templateHtml = readFileSync(templatePath, { encoding: "utf8" });
+const templateHtml = `<!DOCTYPE html>
+<html>
+  <head>
+    <title><!-- Title --> &middot; Uppload</title>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/purecss@1.0.1/build/pure-min.css"
+      integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="https://unpkg.com/uppload/dist/uppload.css" />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/uppload/dist/themes/light.css"
+    />
+    <style>
+      a {
+        color: #0078e7;
+      }
+    </style>
+  </head>
+  <body>
+    <div
+      style="padding: 2rem; max-width: 800px; margin: auto; line-height: 1.5"
+    >
+      <main>
+        <!-- Contents -->
+        <p style="opacity: 0.5">Click on the button below to try this demo.</p>
+        <button class="pure-button pure-button-primary">
+          Upload
+        </button>
+      </main>
+      <footer style="margin-top: 4rem">
+        <h2>Uppload</h2>
+        <p>
+          Uppload is a better JavaScript image uploader. It's highly
+          customizable with 30+ plugins, completely free and open-source, and
+          can be used with any file uploading backend.
+        </p>
+        <a href="https://github.com/elninotech/uppload" class="pure-button"
+          >GitHub</a
+        >
+        <a href="https://uppload.js.org" class="pure-button">Docs</a>
+      </footer>
+    </div>
+    <script src="index.ts"></script>
+  </body>
+</html>`;
 
 const readmePath = join(".", "README.md");
 try {
