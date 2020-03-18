5931c04319d41f2ead9c2cf008eb9b2e5b697431
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-18 22:25:04 +0530

    :sparkles: Add link to sandbox, repo

--- scripts/add-template.js
+++ scripts/add-template.js
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync } = require("fs");
-const { join } = require("path");
+const { join, resolve } = require("path");
 const marked = require("marked");
 
 const templatePath = join(__dirname, "..", "template.html");
@@ -11,9 +11,16 @@ try {
   const readmeHtml = marked(readmeMd.toString())
     .split("\n", 2)
     .join("\n");
+  const exampleName = resolve(".")
+    .split("/")
+    .pop();
   templateHtml = templateHtml
     .toString()
-    .replace("<!-- Contents -->", readmeHtml);
+    .replace(
+      "<!-- Contents -->",
+      readmeHtml +
+        `<p><strong><a href="https://github.com/elninotech/uppload-examples/tree/master/examples/${exampleName}">View the source code</a></strong> of this example or <a href="https://codesandbox.io/s/github/elninotech/uppload-examples/tree/master/examples/${exampleName}">Try it on CodeSandbox</a></p>`
+    );
   const readmeTitle = readmeMd.split("\n", 1)[0].replace("# ", "");
   templateHtml = templateHtml.replace("<!-- Title -->", readmeTitle);
 } catch (error) {}
