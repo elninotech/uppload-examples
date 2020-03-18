c27c59b6e3d3b5aebdb6a2bd4c68140da67dad72
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 01:59:21 +0530

    :sparkles: Add local preview example

---
+++ examples/local-preview/index.ts
@@ -0,0 +1,28 @@
+import { Uppload, en, Local, Preview } from "uppload";
+
+const uploader = new Uppload({
+  lang: en,
+  call: ".pure-button-primary",
+  uploader: (file, updateProgress) =>
+    new Promise(resolve => {
+      console.log("Uploading file...", file);
+      setTimeout(() => resolve(window.URL.createObjectURL(file)), 2750);
+      let progress = 0;
+      const interval = setInterval(() => {
+        if (progress > 99) clearInterval(interval);
+        if (updateProgress) updateProgress(progress++);
+      }, 25);
+    })
+});
+
+// Services
+uploader.use([new Local()]);
+
+// Effects
+uploader.use([new Preview()]);
+
+// Logging
+console.log(uploader);
+uploader.on("*", (...params: any[]) => {
+  console.log(params);
+});
