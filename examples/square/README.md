663cde8e4b386703170a021718e615f780c1a86c
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 02:22:41 +0530

    :sparkles: Add crop to square example

---
+++ examples/square/README.md
@@ -0,0 +1,27 @@
+# Crop to Square
+
+This Uppload example shows how users can crop an image before uploading, particularly useful for cropping profile pictures to squares.
+
+## 🛠 Development
+
+This example uses the [Parcel](https://github.com/parcel-bundler/parcel) bundler:
+
+To start a local development server with HMR:
+
+```bash
+npm run start
+```
+
+To build a production version:
+
+```
+npm run build
+```
+
+## 🐛 Issues
+
+If you want to report a problem or suggestion with Crop to Square, please open an issue in the main [elninotech/uppload](https://github.com/elninotech/uppload) repo and add the label "examples".
+
+## 📄 License
+
+[MIT](https://github.com/elninotech/uppload-examples/blob/master/LICENSE) © [El Niño](https://elnino.tech)
---
+++ examples/square/index.ts
@@ -0,0 +1,33 @@
+import { Uppload, en, Local, Crop } from "uppload";
+import "uppload/dist/uppload.css";
+import "uppload/dist/themes/light.css";
+
+const uploader = new Uppload({
+  lang: en,
+  call: ".pure-button-primary",
+  bind: ".uppload-image",
+  value: "https://via.placeholder.com/150x150",
+  inline: true,
+  uploader: (file, updateProgress) =>
+    new Promise(resolve => {
+      console.log("Uploading file...", file);
+      setTimeout(() => resolve(window.URL.createObjectURL(file)), 2750);
+      let progress = 0;
+      const interval = setInterval(() => {
+        if (progress > 99) clearInterval(interval);
+        if (updateProgress) updateProgress(progress++);
+      }, 25);
+    })
+});
+
+// Services
+uploader.use([new Local()]);
+
+// Effects
+uploader.use([new Crop({ aspectRatio: 1 })]);
+
+// Logging
+console.log(uploader);
+uploader.on("*", (...params: any[]) => {
+  console.log(params);
+});
