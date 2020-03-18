2cbf8ac4731d86a4c0e377b763b33bc672e37151
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-18 22:12:13 +0530

    :sparkles: Add example package

--- examples/meta/index.ts
+++ examples/meta/index.ts
@@ -1,6 +1,13 @@
-import { Uppload, en } from "uppload";
+import { Uppload, en, Local, Instagram } from "uppload";
 
 const uploader = new Uppload({
   lang: en,
   call: ".pure-button-primary"
 });
+
+uploader.use([new Local(), new Instagram()]);
+uploader.on("*", (...params: any[]) => {
+  console.log(params);
+});
+
+console.log(uploader);
